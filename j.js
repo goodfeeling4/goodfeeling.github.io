d05a123cdd3c545f1b670c3ca8ff76d2655a5dc7
goodfeeling4 <mdshahnebaj649@gmail.com>
2024-12-29 16:58:21 +0530

    javascript

---
+++ j.js
@@ -0,0 +1,13 @@
+console.log("begin js with marvelous feeling");
+
+
+function playSong() {
+    const audio = new Audio("Arash - Broken Angel.mp3");
+    audio.play();
+  }
+  // playSong();
+  let pp = document.getElementsByClassName("he")
+   pp = addEventListener("click",playSong());
+   
+   
+
